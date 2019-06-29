c1a6390bad849d07a32b60ef092fc65e41506b29
yaizaaaa <yaiza0521@gmail.com>
2019-06-29 23:58:49 +0900

    be able to recieve slack api challenge post

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const io = require('socket.io')(http);
 const PORT = process.env.PORT || 7000;
 app.use(express.static(__dirname + '/public'));
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
 app.get('/' , function(req, res){
   res.sendFile(__dirname+'/index.html');
 });
@@ -14,6 +18,20 @@ io.on('connection',function(socket){
       console.log('message: ' + msg);
       io.emit('message', msg);
   });
+});
+
+app.post('/slack', function (req, res) {
+
+  const { type, event } = req.body;
+
+  if (type === 'challenge') {
+
+  } else if (type === 'event_callback') {
+      io.emit('message', event.txt);
+  }
+
+  res.status(200).json(req.body);
+
 });
 
 http.listen(PORT, function(){
